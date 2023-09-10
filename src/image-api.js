a6c900259b52ca28d6c17ae15299e14d9906af7a
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-10 14:07:56 +0200

    fixed build

--- src/image-api.js
+++ src/image-api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const API_KEY = '39344884-58dfe21e72d55086ae867b0a2';
 
 export function searchImages(value) {
-  axios.get('https://pixabay.com/api/', {
+  return axios.get('https://pixabay.com/api/', {
     params: {
       key: API_KEY,
       q: value,
