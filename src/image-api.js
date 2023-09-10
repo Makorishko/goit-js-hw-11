4c935f9073a15bc7dd4689a69bbe5545095f1ad1
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-09-10 14:05:43 +0200

    fix

--- src/image-api.js
+++ src/image-api.js
@@ -1,24 +1,14 @@
-import axios from "axios";
-
-
 const axios = require('axios');
 const API_KEY = '39344884-58dfe21e72d55086ae867b0a2';
-const API_URL = 'https://pixabay.com/api/';
-const inputValue = document.querySelector("input[name='searchQuery']");
 
-
-
-
-
-
-export function searchImages(value) { 
-    return axios.get('https://pixabay.com/api/', {
-        params: {
-            key: API_KEY,
-            q: value,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-        }
-    });
+export function searchImages(value) {
+  axios.get('https://pixabay.com/api/', {
+    params: {
+      key: API_KEY,
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
 }
